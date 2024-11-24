e395d3fc2d1ab2d8d9fe9b1d07eb07c463cb8856
Raiana Vitória <nanaejico@gmail.com>
2024-11-24 14:27:45 -0300

    Add files via upload

--- telaLogin.py
+++ telaLogin.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from streamlit.components.v1 import html
+import re
 
 st.set_page_config(
     page_title="Reciclare",
-    page_icon="planta.png",
+    page_icon="https://pbs.twimg.com/media/GdKDgEnXYAAKJ7o?format=webp&name=tiny",
     layout="wide"
 )
 
@@ -13,22 +13,43 @@ def add_css(telaLogin):
 
 add_css("telaLogin.css")
 
+database = {
+    "usuario1@email.com": "senha123",
+    "admin@reciclare.com": "admin2024",
+    "contato@site.com": "contato@123",
+    "miguel0205brgg@gmail.com": "miguelSenha"
+}
+
+def is_valid_email(email):
+    email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(email_regex, email) is not None
+
 col1, col2 = st.columns([2, 3])
 
 with col1:
     st.markdown("<h1>Bem vindo de volta!</h1>", unsafe_allow_html=True)
-    st.markdown("<p>Insira as suas credenciais</p>", unsafe_allow_html=True)
-
+    st.markdown("<h3>Insira as suas credenciais</h3>", unsafe_allow_html=True)
+  
     email = st.text_input("Endereço de email", placeholder="Insira seu endereço de email")
-    
+  
     password = st.text_input("Senha", placeholder="Digite sua senha", type="password")
-    st.markdown('<p><a href="#" style="color: #007bff; text-decoration: none;">Esqueci a senha?</a></p>', unsafe_allow_html=True)
+    
+    st.markdown('<p><a href="#" style="color: #007bff; text-decoration: none; font-size: 0.8em;">Esqueci a senha</a></p>', unsafe_allow_html=True)
 
-    lembrar = st.checkbox("Lembrar-se") #vai deixar?
-
+    lembrar = st.checkbox("Lembrar-se")
+  
     if st.button("Login"):
-        st.success("Login realizado com sucesso!")
-
+        if not email or not password:
+            st.error("Por favor, preencha todos os campos.")
+        elif not is_valid_email(email):
+            st.error("Por favor, insira um email válido.")
+        elif email not in database:
+            st.error("Conta não encontrada. Verifique o email ou inscreva-se.")    
+        elif database[email] != password:
+            st.error("Senha inválida.")
+        else:
+            st.success("Login realizado com sucesso!")
+            
     st.markdown('<p class="register">Não tem uma conta? <a href="#">Inscrever-se</a></p>', unsafe_allow_html=True)
 
 with col2:
